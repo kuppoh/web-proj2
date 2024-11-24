c94c7575dcefb803870d096068dbf4ffae25f65e
kuppoh <kwacnang.23@gmail.com>
2024-11-24 01:32:39 -0800

    idk

--- app.js
+++ app.js
@@ -23,11 +23,6 @@ app.use(
   secret: "secret",
   resave: false,
   saveUninitialized: false,
-  cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
-  },
   })
 );
 
